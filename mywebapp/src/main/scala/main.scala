af0c152d392b107b1a0311ef53b4faec5b1f89a6
svanish <anishsv2009@gmail.com>
2014-02-17 15:11:41 +0530

    maven project

---
+++ mywebapp/src/main/scala/main.scala
@@ -0,0 +1,14 @@
+package newproject
+
+import pro.savant.circumflex._, core._, web._, freemarker._
+import java.util.Date
+
+class Main extends Router {
+  val log = new Logger("newproject")
+
+  'currentDate := new Date
+
+  get("/test") = "I'm fine, thanks!"
+  get("/") = ftl("index.ftl")
+
+}
